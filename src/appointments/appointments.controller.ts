9adc668baae4a781735a1801e6841f1470034c08
whyalohadance <lpline51@gmail.com>
2025-09-24 08:44:22 +0300

    Added appointments module with CRUD endpoints

--- src/appointments/appointments.controller.ts
+++ src/appointments/appointments.controller.ts
@@ -1,28 +1,13 @@
-import { Controller, Get, Post, Delete, Param, Body, ParseIntPipe } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
 
 @Controller('appointments')
 export class AppointmentsController {
-  constructor(private readonly service: AppointmentsService) {}
+  constructor(private readonly appointmentsService: AppointmentsService) {}
 
   @Post()
   create(@Body() dto: CreateAppointmentDto) {
-    return this.service.create(dto);
-  }
-
-  @Get()
-  findAll() {
-    return this.service.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.service.findOne(id);
-  }
-
-  @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: number) {
-    return this.service.remove(id);
+    return this.appointmentsService.create(dto);
   }
 }
--- src/appointments/dto/create-appointment.dto.ts
+++ src/appointments/dto/create-appointment.dto.ts
@@ -1,8 +1,23 @@
+import { IsString, IsNotEmpty, IsDateString, IsNumber, Min } from 'class-validator';
+
 export class CreateAppointmentDto {
+  @IsDateString()
   date: string;
+
+  @IsString()
+  @IsNotEmpty()
   roomName: string;
+
+  @IsNumber()
+  @Min(0)
   price: number;
+
+  @IsNumber()
   durationMin: number;
+
+  @IsNumber()
   patientId: number;
+
+  @IsNumber()
   doctorId: number;
 }
